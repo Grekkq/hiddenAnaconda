86270e920d9a9c1eab1f6248f8398eab35552c46
rychu1973-86@o2.pl <rychu1973-86@o2.pl>
2019-11-16 02:46:40 +0100

    Test4

---
+++ WindowsFormsApp1/Widoki Mastaliński/DodawaniekKursu.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace WindowsFormsApp1.Widoki_Mastaliński
+{
+    partial class DodawanieKursu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.date_name = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.line_choose = new System.Windows.Forms.ComboBox();
+            this.c_number = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.hour_name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cancel = new System.Windows.Forms.Button();
+            this.create = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.exit = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.date_name);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.line_choose);
+            this.panel1.Controls.Add(this.c_number);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.hour_name);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.cancel);
+            this.panel1.Controls.Add(this.create);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(2, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(607, 381);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.White;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(134, 177);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(235, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Akceptowany format czasu - DD:MM:RR";
+            // 
+            // date_name
+            // 
+            this.date_name.HideSelection = false;
+            this.date_name.Location = new System.Drawing.Point(20, 174);
+            this.date_name.MaxLength = 4;
+            this.date_name.Name = "date_name";
+            this.date_name.Size = new System.Drawing.Size(109, 20);
+            this.date_name.TabIndex = 19;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(16, 158);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(134, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Podaj  date nowego kursu:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(135, 138);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(213, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Akceptowany format czasu - GG:MM";
+            // 
+            // line_choose
+            // 
+            this.line_choose.FormattingEnabled = true;
+            this.line_choose.Location = new System.Drawing.Point(19, 95);
+            this.line_choose.Name = "line_choose";
+            this.line_choose.Size = new System.Drawing.Size(130, 21);
+            this.line_choose.TabIndex = 15;
+            // 
+            // c_number
+            // 
+            this.c_number.HideSelection = false;
+            this.c_number.Location = new System.Drawing.Point(17, 213);
+            this.c_number.MaxLength = 4;
+            this.c_number.Name = "c_number";
+            this.c_number.Size = new System.Drawing.Size(109, 20);
+            this.c_number.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Podaj numer nowego kursu";
+            // 
+            // hour_name
+            // 
+            this.hour_name.HideSelection = false;
+            this.hour_name.Location = new System.Drawing.Point(20, 135);
+            this.hour_name.MaxLength = 4;
+            this.hour_name.Name = "hour_name";
+            this.hour_name.Size = new System.Drawing.Size(109, 20);
+            this.hour_name.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Podaj  godzine nowego kursu:";
+            // 
+            // cancel
+            // 
+            this.cancel.Location = new System.Drawing.Point(20, 336);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(90, 28);
+            this.cancel.TabIndex = 9;
+            this.cancel.Text = "Anuluj";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.close_on_click);
+            // 
+            // create
+            // 
+            this.create.Location = new System.Drawing.Point(496, 336);
+            this.create.Name = "create";
+            this.create.Size = new System.Drawing.Size(90, 28);
+            this.create.TabIndex = 8;
+            this.create.Text = "Utwórz";
+            this.create.UseVisualStyleBackColor = true;
+            this.create.Click += new System.EventHandler(this.create_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(26, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(375, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Aby utworzyć nowy kurs wypełnij poniższy formularz a następnie kliknij utwórz.";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Window;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(-1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(608, 68);
+            this.label1.TabIndex = 1;
+            this.label1.Text = " \r\n      Tworzenie nowego kursu";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Wybierz linie";
+            // 
+            // exit
+            // 
+            this.exit.BackColor = System.Drawing.Color.Transparent;
+            this.exit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.exit.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.exit.FlatAppearance.BorderSize = 0;
+            this.exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.exit.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.exit.Location = new System.Drawing.Point(561, 2);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(48, 28);
+            this.exit.TabIndex = 21;
+            this.exit.Text = "x";
+            this.exit.UseVisualStyleBackColor = false;
+            this.exit.Click += new System.EventHandler(this.close_on_click);
+            this.exit.MouseEnter += new System.EventHandler(this.hover_exitbutton);
+            this.exit.MouseLeave += new System.EventHandler(this.leave_exitbutton);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.label7.ForeColor = System.Drawing.SystemColors.Window;
+            this.label7.Location = new System.Drawing.Point(17, 5);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(264, 21);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Formularz tworzenia nowego kursu";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // DodawanieKursu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(99)))), ((int)(((byte)(183)))));
+            this.ClientSize = new System.Drawing.Size(611, 414);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "DodawanieKursu";
+            this.Text = "DodawaniekKursu";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.Button create;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox c_number;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox hour_name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox line_choose;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox date_name;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ WindowsFormsApp1/Widoki Mastaliński/DodawaniekKursu.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.Widoki_Mastaliński
+{
+    public partial class DodawanieKursu : Form
+    {
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;                                               // stałe do funkcji przesuwania okna
+        public const int HT_CAPTION = 0x2;                                                      //
+        [System.Runtime.InteropServices.DllImport("user32.dll")]                                // importowanie funkcji do przesuwania okna
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);     // 
+        [System.Runtime.InteropServices.DllImport("user32.dll")]                                //
+        public static extern bool ReleaseCapture();
+
+        private void move_window(object sender, MouseEventArgs e)
+        {                             // metoda do przesuwania okna
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void hover_exitbutton(object sender, EventArgs e)
+        {
+            exit.BackColor = Color.FromArgb(218, 83, 44);
+        }
+
+        private void leave_exitbutton(object sender, EventArgs e)
+        {
+            exit.BackColor = Color.FromArgb(0, 99, 183);
+        }
+
+        public DodawanieKursu()
+        {
+            InitializeComponent();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void turnoff_focus(object sender, EventArgs e)  //wyłączenie fokusu na pola tekstowe przy uruchomieniu formularza
+        {
+            this.ActiveControl = null;
+        }
+        private void close_on_click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void create_Click(object sender, EventArgs e)
+        {
+            if ((string.IsNullOrEmpty(line_choose.Text)))
+            {
+                MessageBox.Show("Nie wypełniono formularza", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (((string.IsNullOrEmpty(date_name.Text)) || (string.IsNullOrEmpty(c_number.Text)) || (string.IsNullOrEmpty(hour_name.Text))))         
+
+            { 
+                MessageBox.Show("Formularz niewypełniony całkowicie.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            else
+            {
+                const string message = "Pomyślnie utworzono nowy pojazd";
+                const string caption = "Sukces";
+                var result = MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    Application.Exit();
+                }
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
+   
