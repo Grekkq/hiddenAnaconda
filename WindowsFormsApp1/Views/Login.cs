a20fbf134657aec4dbb6b8da04a694de000947e5
Grekkq <grekkq97@gmail.com>
2019-11-27 23:13:40 +0100

    wstępne liczenie hasha

---
+++ WindowsFormsApp1/Views/Login.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace hiddenAnaconda.Views {
+    public partial class Login : Form {
+        public Login() {
+            InitializeComponent();
+        }
+
+        private void createButton_Click(object sender, EventArgs e) {
+            string salt = generateSalt();
+            hashTextBox.Text = SHA1(SHA1(passTextBox+salt));
+        }
+
+        private string generateSalt() {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+            var sb = new StringBuilder();
+            foreach (byte b in salt)
+                sb.AppendFormat("{0:x2}", b);
+            return sb.ToString();
+        }
+
+        private string SHA1(string input) {
+            byte[] hash;
+            using (var sha1 = new SHA1CryptoServiceProvider()) {
+                hash = sha1.ComputeHash(Encoding.Unicode.GetBytes(input));
+            }
+            var sb = new StringBuilder();
+            foreach (byte b in hash)
+                sb.AppendFormat("{0:x2}", b);
+            return sb.ToString();
+        }
+    }
+}
---
+++ WindowsFormsApp1/sqlStuff/DbConnectionManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace hiddenAnaconda.sqlStuff {
+    class DbConnectionManager {
+        SqlConnection connection;
+        SqlCommand command;
+
+        public DbConnectionManager() {
+            connection = new SqlConnection();
+            connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=hiddenAnacondaDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandType = CommandType.Text;
+        }
+
+        public string GetUserHash(ref string login) {
+            try {
+                command.CommandText = "select * from TableName where firstname=@firstname and lastname=@lastname";
+            } catch { } finally {
+                connection.Close();
+            }
+            return null;
+        }
+
+        //public bool CheckUserPassword(ref string login, ref string hash) {
+        //    bool returnvalue = false;
+        //    try {
+        //        command.CommandText = "select * from TableName where firstname=@firstname and lastname=@lastname";
+        //        command.Parameters.Add("firstname", SqlDbType.VarChar).Value = firstname;
+        //        command.Parameters.Add("lastname", SqlDbType.VarChar).Value = lastname;
+        //        connection.Open();
+        //        SqlDataReader reader = command.ExecuteReader();
+        //        if (reader.HasRows) {
+        //            while (reader.Read()) {
+
+        //                lastname = reader.GetString(1);
+        //                firstname = reader.GetString(2);
+
+        //                age = reader.GetString(3);
+
+
+        //            }
+        //        }
+        //        returnvalue = true;
+        //    } catch { } finally {
+        //        connection.Close();
+        //    }
+        //    return returnvalue;
+
+
+
+        }
+}
