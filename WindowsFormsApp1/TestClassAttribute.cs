e8d8bf46dace666deaf12c88f0c8b57b16cec1bb
Mateo <mateusz.bielecki1tis@gmail.com>
2019-12-04 23:16:25 +0100

    Usunięcie zbędnych plików z testów automatycznych.

--- WindowsFormsApp1/TestClassAttribute.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-namespace hiddenAnaconda {
-    internal class TestClassAttribute : Attribute {
-    }
-}
--- WindowsFormsApp1/TestContext.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace hiddenAnaconda {
-    public class TestContext {
-    }
-}
