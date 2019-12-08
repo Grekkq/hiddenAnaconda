0ba91f4b879d366d0e8e0de8d016aa7caa5e66f5
Grekkq <grekkq97@gmail.com>
2019-12-08 12:35:53 +0100

    początki implementacji interfejsu generowania raportów

---
+++ WindowsFormsApp1/Models/StopsInOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hiddenAnaconda.Models {
+    class StopInOrder {
+        public string stopName;
+        public string cityName;
+        public int routeNumber;
+        public int order;
+        public StopInOrder(string stopName, string cityName, int routeNumber, int order) {
+            this.stopName = stopName;
+            this.cityName = cityName;
+            this.routeNumber = routeNumber;
+            this.order = order;
+        }
+    }
+}
--- WindowsFormsApp1/Models/TimeTable.cs
+++ WindowsFormsApp1/Models/TimeTable.cs
@@ -15,12 +15,14 @@ namespace hiddenAnaconda.Models {
             dc = new Models.ReportDataContext();
         }
 
+        // Get rodzajKursu at specific day
         private string CheckDayTypeAtDate(DateTime date) {
             var result = dc.dni_kursowanias.Where(dni => dni.od_dnia <= date & dni.do_dnia >= date);
             return result.First().rodzaj_kursu;
         }
 
-        // wyciaga id trasy tylko dla weekendu albo normalnego tygodnia 
+        // Get all trasaId for specific line at specific day
+        // to chyba wyciąga tylko pierwszy przystanek?
         private List<int> GetTrasasNumberAtDay(DateTime date, int linia) {
             var data = from k in dc.kurs
                        from t in dc.trasas
@@ -35,6 +37,7 @@ namespace hiddenAnaconda.Models {
             return list;
         }
 
+        // juz nie pamiętam :/
         public void GetTimeTable(int linia, string przystanek, DateTime date, int kierunek) {
             var petla = from l in dc.linias
                         where l.id_linii == linia
@@ -56,7 +59,8 @@ namespace hiddenAnaconda.Models {
             }
         }
 
-        public void GetAllBusStops(int linia) {
+        // Get every stop for one line no matter what
+        public List<StopInOrder> GetAllBusStops(int linia) {
             var data = from p in dc.przystaneks
                        from t in dc.trasas
                        where t.id_linii == linia && p.id_przystanku == t.id_przystanku
@@ -67,15 +71,33 @@ namespace hiddenAnaconda.Models {
                            t.nr_trasy,
                            t.kolejnosc_przystankow,
                        };
+            List<Models.StopInOrder> stopList = new List<StopInOrder>();
+            Debug.Print("List all stops for line number: {0}", linia);
             foreach (var item in data) {
-                Console.WriteLine("Przystanek: {0}, {1}; Trasa: {2}, Kolejność: {3}", item.miasto, item.nazwa, item.nr_trasy, item.kolejnosc_przystankow);
+            Models.StopInOrder singleStop = new StopInOrder(item.nazwa, item.miasto, item.nr_trasy, item.kolejnosc_przystankow);
+                stopList.Add(singleStop);
+                Debug.Print("\tPrzystanek: {0}, {1}; Trasa: {2}, Kolejność: {3}", item.miasto, item.nazwa, item.nr_trasy, item.kolejnosc_przystankow);
             }
+            return stopList;
         }
 
+        // Save file on disk
         public void SaveToPdf(string content) {
             var htmlToPdf = new HtmlToPdf();
             var pdf = htmlToPdf.RenderHtmlAsPdf(content);
             pdf.SaveAs(Path.Combine(Directory.GetCurrentDirectory(), "Report" + ".pdf"));
+        }
+
+        // Main function generating report
+        // TODO: many things xD
+        public void GenerateReport(int lineName, string dayType, string stopName) {
+            List<StopInOrder> stops = GetAllBusStops(lineName);
+            StringBuilder htmlStops = new StringBuilder();
+            foreach (var stop in stops) {
+                htmlStops.Append(stop.stopName);
+                htmlStops.Append(", ");
+            }
+            SaveToPdf(htmlStops.ToString());
         }
     }
 }
