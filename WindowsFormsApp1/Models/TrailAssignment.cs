1b01d7f687aa0341c94e181499481626c7dd4d06
Grekkq <grekkq97@gmail.com>
2020-01-03 00:32:07 +0100

    do przetestowania działanie sortwoania kursów

--- WindowsFormsApp1/Models/TrailAssignment.cs
+++ WindowsFormsApp1/Models/TrailAssignment.cs
@@ -22,14 +22,32 @@ namespace hiddenAnaconda.Models {
             toAdd.id_linii = LineNumber;
             toAdd.id_trasy = new SharedView().GetFirstStopInTrail(TrailNumber, LineNumber).id_trasy;
             toAdd.id_czasu_odjazdu = GetArrivalTime(toAdd.id_trasy).id_czasu_odjazdu;
-            // TODO: jakoś obliczać który kurs danego dnia
-            toAdd.ktory_kurs_danego_dnia = 0;
+            toAdd.ktory_kurs_danego_dnia = GetNumberOfTrailAssignments() + 1;
             toAdd.rodzaj_kursu = DayType;
             AddTrailAssignmentToDb(toAdd);
+            SortArrivalTime();
         }
 
         private czas_odjazdu GetArrivalTime(int TrailId) {
             return db.czas_odjazdus.Where(c => c.id_trasy.Equals(TrailId)).Single();
+        }
+
+        private int GetNumberOfTrailAssignments() {
+            return db.kurs.Where(k => k.id_linii.Equals(LineNumber)).Count();
+        }
+
+        // TODO: Sortować jakoś te kursy :D
+        private void SortArrivalTime() {
+            var data = from k in db.kurs
+                       from a in db.czas_odjazdus
+                       where k.id_czasu_odjazdu == a.id_czasu_odjazdu && k.id_linii == LineNumber && k.rodzaj_kursu == DayType
+                       orderby a.czas_odjazdu1 select k;
+            int i = 0;
+            foreach (var item in data) {
+                item.ktory_kurs_danego_dnia = i;
+                i++;
+            }
+            db.SubmitChanges();
         }
 
         private void AddTrailAssignmentToDb(kur toAdd) {
