f7bce4edfb34d997cbe7d746fb44275c7bde3083
Grekkq <grekkq97@gmail.com>
2020-01-14 19:03:45 +0100

    backend zmiany bazy danych

---
+++ WindowsFormsApp1/Models/ChangeDb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace hiddenAnaconda.Models {
+    static class ChangeDb {
+        public static void ChangeCurrentDb() {
+
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var connectionStringsSection = (ConnectionStringsSection)config.GetSection("connectionStrings");
+            connectionStringsSection.ConnectionStrings["hiddenAnaconda.Properties.Settings.hiddenAnacondaDBConnectionString"].ConnectionString = "Data Source=blah;Initial Catalog=blah;UID=blah;password=blah";
+            config.Save();
+            ConfigurationManager.RefreshSection("connectionStrings");
+        }
+    }
+}
